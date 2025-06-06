881fcd4c2eb9a73add761a10647248432fd07915
Matheus Dias <matheus.dias@fullstacklabs.co>
2025-06-06 13:25:44 -0300

    Add App basic components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
 import './App.css'
+// Components
+import PortfolioHeader from './components/PortfolioHeader'
+import PortfolioFooter from './components/PortfolioFooter'
+// Views
+import HomeView from './components/views/HomeView'
+import AboutView from './components/views/AboutView'
+import CareerView from './components/views/CareerView'
+import ProjectsView from './components/views/ProjectsView'
 
 function App() {
   return (
     <>
+      <div className="portfolio-container">
+        <PortfolioHeader />
+
+        <HomeView />
+        <AboutView />
+        <CareerView />
+        <ProjectsView />
+
+        <PortfolioFooter />
+      </div>
     </>
   )
 }
